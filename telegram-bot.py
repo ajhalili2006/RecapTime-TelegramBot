987810fb9e241613ceedcd8e9c81b49b01f915f8
Glitch (flask-python3) <none>
2019-09-05 09:44:44 +0000

    🏵💕 Checkpoint ./telegram-bot.py:4790460/607

--- telegram-bot.py
+++ telegram-bot.py
@@ -23,7 +23,16 @@ def send_welcome(message):
 	bot.reply_to(message,"*Howdy, welcome to Recap Time bot!*\n\nTo get started using me, see /quickstart or /helpfor the full scoop.", parse_mode="markdown")
   
 @bot.message_handler(commands=['support'])
-def send_
-  bot.send
+def support_links(message):
+  bot.send_message(message, "To access")
+  
+@bot.inline_handler(lambda query: query.query == 'experimental_search')
+def query_text(inline_query):
+    try:
+        r = types.InlineQueryResultArticle('1', 'Result', types.InputTextMessageContent('Inline search under'))
+        r2 = types.InlineQueryResultArticle('2', 'Result2', types.InputTextMessageContent('Result message2.'))
+        bot.answer_inline_query(inline_query.id, [r, r2])
+    except Exception as e:
+        print(e)
   
 bot.polling(print("Logged in as " + Telegram_BotUsername + " on api.telegram.org. If there's error on the bot token, please change it inside .env file."))
