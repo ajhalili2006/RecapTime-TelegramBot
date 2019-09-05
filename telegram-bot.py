1fa44160a73a73631ce0cae3d7df6894343cae3c
Glitch (flask-python3) <none>
2019-09-05 07:31:30 +0000

    🎻🍬 Checkpoint ./telegram-bot.py:4790460/17

--- telegram-bot.py
+++ telegram-bot.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 import telebot
 
-WEBHOOK_HOST = '<https://garnet'
+WEBHOOK_HOST = 'https://garnet-crate.glitch.me'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
 
 # Get the path to the directory this file is in
