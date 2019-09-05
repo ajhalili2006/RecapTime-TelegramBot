e8e23da66782da85de0f0325052cc45f9882ead9
Glitch (flask-python3) <none>
2019-09-05 08:07:31 +0000

    🚶🐿 Checkpoint ./telegram-bot.py:4790460/1010

--- telegram-bot.py
+++ telegram-bot.py
@@ -1,11 +1,9 @@
 import os
 from dotenv import load_dotenv
 import telebot
-
-WEBHOOK_HOST = 'https://garnet-crate.glitch.me'
-WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
-WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (API_TOKEN)
+import logging
+import time
+import flask
 
 # Get the path to the directory this file is in
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
@@ -17,18 +15,19 @@ Telegram_Token = os.getenv("TOKEN")
 
 bot = telebot.TeleBot(Telegram_Token)
 
+app = flask.Flask(__name__)
+
+WEBHOOK_HOST = 'https://garnet-crate.glitch.me'
+WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (Telegram_Token)
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
 	bot.reply_to(message, "*Howdy, welcome to Recap Time bot!*" + "\n" + "\n" +
               "To")
-  
-bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
-                certificate=open(WEBHOOK_SSL_CERT, 'r'))
-
-# Start flask server
-app.run(host=WEBHOOK_LISTEN,
-        port=WEBHOOK_PORT,
-        ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
-        debug=True)
   
 print("Logged in as " + Telegram_Token + " on api.telegram.org")
