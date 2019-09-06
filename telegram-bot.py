e0fedb8bb439cb085a01138aa283d7c331b9538c
Glitch (garnet-crate) <none>
2019-09-06 07:12:12 +0000

    🐣💠 Checkpoint ./telegram-bot.py:4790460/702

--- telegram-bot.py
+++ telegram-bot.py
@@ -28,25 +28,33 @@ import flask
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(BASEDIR, '.env'))
 
-# Then setup the environmental variables
+# Then setup the environmental variables. Make sure these are available from .env file.
+# Code example:
+# CONFIG_VAR-SHPRTCUT_HERE = os.getenv("ENV_VARIABLE_HERE")
 Telegram_Token = os.getenv("TOKEN")
 Telegram_BotUsername = os.getenv("BOT_USERNAME")
 
-# After settings up, start coding!
+# After we finish on environmental variables, try these first.
 bot = telebot.TeleBot(Telegram_Token)
-
 app = flask.Flask(__name__)
 
+# We include some code like these for deep-linking.
+def command_extractor(text):
+    # Extracts the unique_code from the sent /start command.
+    return text.split()[1] if len(text.split()) > 1 else None
+
+# Now, set up the last part: the commands and others...
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
 	bot.reply_to(message,"*Howdy, welcome to Recap Time bot!*\n\nTo get started using me, see /quickstart or /help for the full scoop.\n\nTo view your settings", parse_mode="markdown")
   
-@bot.message_handler(commands=['support'])
+@bot.message_handler(commands=['support, help'])
 def support_links(message):
   bot.send_message(message, "To access support page")
   
 @bot.message_handler(content_types=['text'])
 def command_not_found(message):
-	bot.reply_to(message, "Something went wrong on our side. Please try again or see /help for details.\n\nFor more information about *404 Command Not Found*, press the button below", parse_mode='markdown')
+	bot.reply_to(message, "Something went wrong on our side. Please try again or see /help for details.\n\nFor more information about *404 Command Not Found*, press the button below or [click here](https://t.me/RecapTime_bot?start=help_404commandnotfound)", parse_mode='markdown')
 
+# When ready, use Polling. If Webhooks, see
 bot.polling(print("Logged in as " + Telegram_BotUsername + " on api.telegram.org. If there's error on the bot token, please change it inside .env file."))
