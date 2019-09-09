85dc45a931a0e814752a5aa30fb843225082a248
Glitch (garnet-crate) <none>
2019-09-09 09:20:14 +0000

    👹🏘 Checkpoint ./telegram-bot.py:4790460/50

--- telegram-bot.py
+++ telegram-bot.py
@@ -15,8 +15,8 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-#
+# -----END LICENSE INFO-----
+# -----SUPPORT INFO-----
 #
 
 # Let start boot up the whole program.
