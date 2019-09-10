ce1a248bc6ff718f977c6e675d4f81fe80f0aa91
Glitch (garnet-crate) <none>
2019-09-10 03:33:08 +0000

    🎀📅 Checkpoint ./telegram-bot.py:4790460/2402

--- telegram-bot.py
+++ telegram-bot.py
@@ -48,43 +48,5 @@ print("We found the file!")
 Telegram_Token = os.getenv("TOKEN")
 Telegram_BotUsername = os.getenv("BOT_USERNAME")
 
-# After we finish on environmental variables, do these business:
-bot = telebot.TeleBot(Telegram_Token)
-app = flask.Flask(__name__)
-
-# We include some code like these for deep-linking.
-
-
-# Now, set up the last part: the commands and others...
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-	bot.reply_to(message,"*Howdy, welcome to Recap Time bot!*\n\nTo get started using me, see /quickstart or /help for the full scoop.\n\nTo view your settings", parse_mode="markdown")
-  
-# For /help command, we use Inline Keyboard to see options below the text message.
-@bot.message_handler(commands=['help'])
-def send_welcome(message):
-    msg = bot.reply_to(message, "**Welcome to the mini Help Center!**" + "\n" + "\n" +
-                      "Please select an option to navigate around the Help Center.", parse_mode="markdown")
-    markup = types.InlineKeyboardMarkup()
-    contact_support = types.InlineKeyboardButton('Contact Support', callback_data="contact_support")
-    open_hc = types.InlineKeyboardButton('Open Help Center', callback_data="open_hc")
-    commands_help = types.InlineKeyboardButton('Commands Help', callback_data="commands_help")
-    markup.row(cntact_support)
-    markup.row(itembtnv)
-    markup.row(itembtnc)
-
-@bot.callback_query_handler(lambda query: query.data == "contact_support")
-def process_callback_1(query):
-  pass
-
-@bot.callback_query_handler(lambda query: query.data in ["ds", "sds"])
-def process_callback_2(query):
-  pass
-    
-# For other messages that the bot can't process, we use the fallback message for that case.
-#
-
-    
-# When ready, use Polling. If Webhooks, see docs for info.
-print("The whole Python code is in good state, as what the Python test results said. We're connecting to Telegram servers...")
-bot.polling(print("Logged in as " + Telegram_BotUsername + " on api.telegram.org. Everything will gone right, unless you update your code and do the wrong things. Congrats!"))
+tb = telebot.TeleBot(Telegram_Token)
+print("Connected to servers! You're now signed in as " + Telegram_BotUsername)
