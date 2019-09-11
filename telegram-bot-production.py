84efaf55ce362663bbef7aa9f859794a17227689
Glitch (garnet-crate) <none>
2019-09-11 07:48:04 +0000

    🏐🏖 Checkpoint ./telegram-bot-production.py:4790460/3649 ./.gitignore:4790460/11

--- telegram-bot-production.py
+++ telegram-bot-production.py
@@ -70,49 +70,33 @@ def command_not_found(message):
   "sent an command or message but neither the server or the bot itself doesn't understand it.")
 
 # Speaking of Inline mode, we use some code samples from
-@bot.inline_handler(lambda query: query.query == 'text')
+@bot.inline_handler(lambda query: query.query == 'samples_github-repo:eternnoir/pyTelegramBotAPI')
 def query_text(inline_query):
     try:
         r = types.InlineQueryResultArticle('1', 'Result1', types.InputTextMessageContent('hi'))
         r2 = types.InlineQueryResultArticle('2', 'Result2', types.InputTextMessageContent('hi'))
-        bot.answer_inline_query(inline_query.id, [r, r2])
-    except Exception as e:
-        print(e)
-
-
-@bot.inline_handler(lambda query: query.query == 'photo: samples_github-repo:eternnoir/pyTelegramBotAPI')
-def query_photo(inline_query):
-    try:
-        r = types.InlineQueryResultPhoto('1',
+        r3 = types.InlineQueryResultPhoto('1',
                                          'https://raw.githubusercontent.com/eternnoir/pyTelegramBotAPI/master/examples/detailed_example/kitten.jpg',
                                          'https://raw.githubusercontent.com/eternnoir/pyTelegramBotAPI/master/examples/detailed_example/kitten.jpg',
                                          input_message_content=types.InputTextMessageContent('hi'))
-        r2 = types.InlineQueryResultPhoto('2',
+        r4 = types.InlineQueryResultPhoto('2',
                                           'https://raw.githubusercontent.com/eternnoir/pyTelegramBotAPI/master/examples/detailed_example/rooster.jpg',
                                           'https://raw.githubusercontent.com/eternnoir/pyTelegramBotAPI/master/examples/detailed_example/rooster.jpg')
-        bot.answer_inline_query(inline_query.id, [r, r2], cache_time=1)
-    except Exception as e:
-        print(e)
-
-
-@bot.inline_handler(lambda query: query.query == 'video')
-def query_video(inline_query):
-    try:
-        r = types.InlineQueryResultVideo('1',
+        r5 = types.InlineQueryResultVideo('1',
                                          'https://github.com/eternnoir/pyTelegramBotAPI/blob/master/tests/test_data/test_video.mp4?raw=true',
                                          'video/mp4', 'Video',
                                          'https://raw.githubusercontent.com/eternnoir/pyTelegramBotAPI/master/examples/detailed_example/rooster.jpg',
                                          'Title'
                                          )
-        bot.answer_inline_query(inline_query.id, [r])
+        bot.answer_inline_query(inline_query.id, [r, r2, r3, r4, r5, r6], cache_time=1)
     except Exception as e:
         print(e)
-
 
 @bot.inline_handler(lambda query: len(query.query) is 0)
 def default_query(inline_query):
     try:
-        r = types.InlineQueryResultArticle(, 'For help in using my inline search, click me.', types.InputTextMessageContent('To get started using me, you can...'))
+        r = types.InlineQueryResultArticle(, 'For help in using my inline search, click me.', types.InputTextMessageContent('To get started using me, you can try the following' +
+        '\n' + '\n' + ))
         bot.answer_inline_query(inline_query.id, [r])
     except Exception as e:
         print(e)
